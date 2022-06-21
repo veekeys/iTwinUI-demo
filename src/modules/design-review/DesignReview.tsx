36736bfb8020629f9aaa683731bddb02f8629b61
veekeys <81580355+veekeys@users.noreply.github.com>
2022-06-21 14:04:29 +0300

    change

--- src/modules/design-review/DesignReview.tsx
+++ src/modules/design-review/DesignReview.tsx
@@ -1,18 +1,19 @@
-import { Text } from "@itwin/itwinui-react";
+import { Subheading, Text } from "@itwin/itwinui-react";
 import FileUploadDemo from "./components/FileUploadDemo";
 import SelectiModelDemo from "./components/SelectiModelDemo";
 import PreviousSessionsDemo from "./components/PreviousSessionsDemo";
 import SessionsTableDemo from "./components/SessionsTableDemo";
 import SessionsSearchDemo from "./components/SessionsSearchDemo";
+import { Grid } from "@itwin/itwinui-layouts-react";
 
 const DesignReview = () => {
   return (
     <>
       <Text variant="title">iTwin Design Review - Ad Hoc Reviews</Text>
-      <Text variant="subheading">New session</Text>
+      <Subheading>New session</Subheading>
       <FileUploadDemo />
       <SelectiModelDemo />
-      <Text variant="subheading">Previous session</Text>
+      <Subheading>Previous session</Subheading>
       <PreviousSessionsDemo />
       <Text variant="subheading">Sessions</Text>
       <SessionsSearchDemo />
